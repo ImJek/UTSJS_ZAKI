ec04a7d29e0ed919617fbfe303768608a32de795
ImJek <alfaruky67@gmail.com>
2023-03-16 11:08:49 +0700

    UTSJS_ZAKI

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 class jenishewan {
-    constructor(anggora, kaki , harga){
+    constructor(anggora, kaki , harga, warna){
         this.jeniskucing = anggora;
         this.kaki = kaki;
         this.harga = harga;
+        this.warna = warna;
     }
     panggilkan(){
         return " panggilkan si " + this.jeniskucing + " buat dia cucui piring "
     }
 }
 
-var kucingjeki = new jenishewan ('kucing dapur, 4 ,500000')
+var kucingjeki = new jenishewan ('kucing dapur, 4 ,500000, putih hitam')
 console.log(kucingjeki.jeniskucing);
 console.log(kucingjeki.kaki);
 console.log(kucingjeki.harga);
+console.log(kucingjeki.warna);
 
 console.log(kucingjeki.panggilkan())
